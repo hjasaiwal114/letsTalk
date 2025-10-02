b1724c3ed77243452481d85a87211c58672cf7b9
himanshu <hjasaiwal114@gmail.com>
2025-10-03 00:22:46 +0530

    feat(css): add animations (blink, loading-bar) and effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router";
+import { Home } from "./pages/Home";
 
 function App() {
 
   return (
-    <></>
+    <div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
